216a0b84acc6813cccf3e692ec061b0fbffd507a
James Felton <31760871+feijiahua@users.noreply.github.com>
2021-05-31 17:29:58 +0800

    Update pyping.py
    添加超时修饰器

--- pyping.py
+++ pyping.py
@@ -1,12 +1,43 @@
+import os
 import sys
 import time
+import signal
+import errno
 import socket
 import struct
 import getopt
 import argparse
+from functools import wraps
 from impacket import ImpactDecoder, ImpactPacket
 
+class TimeoutError(Exception):
+    pass
 
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+        return wraps(func)(wrapper)
+    return decorator
+
+def log(func):
+    def wrapper(*args, **kargs):
+        print('log')
+        return func(*args, **kargs)
+    
+    return wrapper
+
+
+@timeout(5)
 def send_and_recv(dst):
     ip = ImpactPacket.IP()
     icmp = ImpactPacket.ICMP()
@@ -38,19 +69,39 @@ def send_and_recv(dst):
 
 
 def pyping():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ht")
-    except getopt.GetoptError:
-        print('Inout error')
-        sys.exit(2)
+    always = False
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print('pyping <dst ip>')
-            sys.exit(0)
+    parser = argparse.ArgumentParser(description="use for pyping")
+    # 必选参数
+    parser.add_argument('ip', help='the dst ip')
+    # 可选参数
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s version: v 0.01', help='show the version')
+    # 互斥参数
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-c', '--count', help='Stop after sending count ECHO_REQUEST packets. With deadline option, ping waits for count ECHO_REPLY packets, until the timeout expires.')
+    group.add_argument('-t', action="store_true")
 
-    dst = args[0]
-    send_and_recv(dst)
+    args = parser.parse_args()
+    dst = args.ip
+
+    if args.t:
+        while True:
+            try:
+                send_and_recv(dst)
+                time.sleep(1)
+            except TimeoutError:
+                print('timeout')
+    elif args.count:
+        cnt = int(args.count)
+        while cnt:
+            try:
+                send_and_recv(dst)
+                time.sleep(1)
+            except TimeoutError:
+                print('timeout')
+            finally:
+                cnt -= 1
+    
 
 
 class Ping():
