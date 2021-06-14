d7428c630b66e8c7b3fd097d6f9ca1569391c914
root <root@localhost.localdomain>
2021-06-15 00:02:56 +0800

    modify

--- main_ui.py
+++ main_ui.py
@@ -16,18 +16,17 @@ class UI():
         return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
     def item_chosen(self, button, choice):
-        choice_widget = getattr(self, choice.lower())()
-        self.main.original_widget = choice_widget  # urwid.Filler(choice_ui)
+        self.main.original_widget = getattr(
+            self, choice.lower())()
 
-    def main(self):
-        main_widget = urwid.Padding(
-            self.menu(self.title, self.choices), left=2, right=2)
-        return main_widget
+    def main_menu(self):
+        return self.menu(self.title, self.choices)
 
     def icmp(self):
         icmp_choices = u'Ping Customize'.split()
         body = [urwid.Text(u'ICMP'), urwid.Divider()]
         for c in icmp_choices:
+
             button = urwid.Button(c)
             urwid.connect_signal(button, 'click', self.item_chosen, c)
             body.append(urwid.AttrMap(button, None, focus_map='reversed'))
@@ -38,12 +37,22 @@ class UI():
 
         return urwid.Padding(choices_list)
 
-    def send_program(self, button):
+    def ping(self):
+        src = urwid.Edit(u'src: ')
+        dst = urwid.Edit(u'dst: ')
+        data = urwid.Edit(u'data: ')
+        send = urwid.Button(u'send')
+        back = urwid.Button(u'back')
+
+        urwid.connect_signal(back, 'click', self.item_chosen, 'main_menu')
+
+        return urwid.ListBox([src, dst, data, send, back])
+
+    def send(self, button):
         pass
 
     def return_main(self, button, choice):
-        self.main.original_widget = urwid.Padding(
-            self.menu(self.title, self.choices), left=2, right=2)
+        self.main.original_widget = self.menu(self.title, self.choices)
 
     def start(self):
         self.main = urwid.Padding(
